b499ee773b0b6e776219c47a7bf5d19638a1dc82
Austin Preston <apreston3@deimos.cs.unm.edu>
2021-09-23 18:24:48 -0600

    inital commit

---
+++ sudoku.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+  int charManager;
+  int workingNum = 0;
+  int charCounter = 0;
+  
+  while((charManager = getchar()) != EOF)
+    {
+      charCounter++;
+      if (charManager >= '0' && charManager <= '9')
+	{
+	  workingNum = charManager - '0';
+	  value = value * 10;
+	  value = value + workingNum
+	}
+      if (charManager == '.' || charManager >= 
+    }
+}
