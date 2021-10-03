91f39caddb7b49570f45f8fbf325cdc18683d699
Austin Preston <apreston3@deimos.cs.unm.edu>
2021-10-03 17:01:32 -0600

    Input reading improved

--- sudoku.c
+++ sudoku.c
@@ -1,25 +1,41 @@
 #include <stdio.h>
 
+void makePuzzle(int puzzle[9][9]);
+int isPuzzle();
+
 int main()
 {
   int charManager;
   int charCounter = 0;
+  int emptySpaces = 0;
   int errorCounter = 0;
+  int sameNum = 0;
   
   while((charManager = getchar()) != EOF)
     {
+      emptySpaces++;
       charCounter++;
-      if (charManager != '.' && charManager > '9')
+      if (charManager == '.' || charManager == '\n' ||
+	  (charManager >= '1' && charManager <= '9'))
 	{
 	  putchar(charManager);
-          errorCounter++;
-	}
-		
+	}		
       else
 	{
 	  putchar(charManager);
+	  errorCounter++;
 	}
       if (charManager == '\n' && charCounter != 82)
+	{
+	  errorCounter++;
+	}
+      
+      if (charManager >= '1' && charManager <= '9')
+	{
+	  sameNum = charManager;
+	  
+	}
+      if (sameNum == charManager && emptySpaces <= 9)
 	{
 	  errorCounter++;
 	}
@@ -33,7 +49,20 @@ int main()
 	  printf("\n");
 	  charCounter = 0;
 	}
+      if (emptySpaces == 10)
+	{
+	  emptySpaces = 0;
+	}
     }
   return 0;
 }
+
+int isPuzzle()
+{
+  return 0;
+}
+
+
+
+
 
