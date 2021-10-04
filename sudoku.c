278a3a4db941ccb04366a031a0e2177d37ca9932
Austin Preston <apreston3@phoebe.cs.unm.edu>
2021-10-04 15:17:52 -0600

    Moving long code into seperate functions

--- sudoku.c
+++ sudoku.c
@@ -2,65 +2,11 @@
 
 void makePuzzle(int puzzle[9][9]);
 int isPuzzle();
+int initialChecks();
 
 int main()
 {
-  int charManager;
-  int charCounter = 0;
-  int emptySpaces = 0;
-  int errorCounter = 0;
-  int numCounter = 0;
-  int sameNum = 0;
-  
-  while((charManager = getchar()) != EOF)
-    {
-      emptySpaces++;
-      charCounter++;
-      if (charManager == '.' || charManager == '\n' ||
-	  (charManager >= '1' && charManager <= '9'))
-	{
-	  putchar(charManager);
-	}		
-      else
-	{
-	  putchar(charManager);
-	  errorCounter++;
-	}
-      if (charManager == '\n' && charCounter != 82)
-	{
-	  errorCounter++;
-	}
-      
-      if (charManager >= '1' && charManager <= '9')
-	{
-	  sameNum = charManager;
-	  
-	}
-      if (sameNum == '2' && emptySpaces <= 9)
-	{
-	  numCounter++;
-	}
-      if (numCounter > 2)
-	{
-	  errorCounter++;
-	}
-      
-      if (charManager == '\n')
-	{
-	  if (errorCounter > 0)
-	    {
-	      printf("Error\n");
-	      errorCounter = 0;
-	    }
-	  printf("\n");
-	  charCounter = 0;
-	}
-      if (emptySpaces == 10)
-	{
-	  numCounter = 0;
-	  emptySpaces = 0;
-	}
-    }
+  initialChecks();
   return 0;
 }
 
@@ -69,7 +15,39 @@ int isPuzzle()
   return 0;
 }
 
+int initialChecks()
+{
+  int charManager;
+  int charCounter = 0;
+  int errorCounter = 0;
 
-
-
-
+  while((charManager = getchar()) != EOF)
+  {
+    charCounter++;
+    if (charManager == '.' || charManager == '\n' ||
+        (charManager >= '1' && charManager <= '9'))
+    {
+      putchar(charManager);
+    }
+    else
+    {
+      putchar(charManager);
+      errorCounter++;
+    }
+    if (charManager == '\n' && charCounter != 82)
+    {
+      errorCounter++;
+    }
+    if (charManager == '\n')
+    {
+      if (errorCounter > 0)
+      {
+        printf("Error\n");
+        errorCounter = 0;
+      }
+      printf("\n");
+      charCounter = 0;
+    }
+  }
+  return 0;
+}
